6b75b72efb1f98a0497fa6d80a50d32c53a33f7d
regnat <rg@regnat.ovh>
2019-07-11 20:23:03 +0200

    Add a post-build-hook
    Passing `--post-build-hook /foo/bar` to a nix-* command will cause
    `/foo/bar /nix/store/drv-that/has-been-built.drv -- out1 out2 ... outn`
    to be executed after each build.
    
    This can be useful in particular to upload all the builded artifacts to
    the cache (including the ones that don't appear in the runtime closure
    of the final derivation or are built because of IFD)

--- src/libstore/build.cc
+++ src/libstore/build.cc
@@ -1585,6 +1585,19 @@ void DerivationGoal::buildDone()
            being valid. */
         registerOutputs();
 
+        if (settings.postBuildHook != "") {
+            printMsg(lvlChatty, format("executing post-build hook '%1%'")
+                % settings.postBuildHook);
+            auto outputPaths = drv->outputPaths();
+            Strings args;
+            for (auto outputPath: outputPaths)
+                args.push_front(outputPath);
+            args.push_front("--");
+            args.push_front(drvPath);
+            RunOptions opts(settings.postBuildHook, args);
+            runProgram2(opts);
+        }
+
         if (buildMode == bmCheck) {
             done(BuildResult::Built);
             return;
@@ -2278,6 +2291,7 @@ void DerivationGoal::startBuilder()
         }
         debug(msg);
     }
+
 }
 
 
@@ -4506,3 +4520,4 @@ void LocalStore::repairPath(const Path & path)
 
 
 }
+
