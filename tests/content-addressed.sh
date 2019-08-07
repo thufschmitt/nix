8e072e5ee66736ee73e22cf9c388c7acf52db39a
regnat <rg@regnat.ovh>
2019-08-07 11:32:21 +0200

    Update the test case

--- tests/content-addressed.sh
+++ tests/content-addressed.sh
@@ -1,7 +1,16 @@
 source common.sh
 
 clearStore
+clearCache
 
-# cgdb -- --init-command gdbinit --args nix-build -j1 -vvvvv ./content-addressed.nix
-nix-build -j1 -vvvvv ./content-addressed.nix
+outPath=$(nix-build ./content-addressed.nix)
 
+# Make sure that the registration was correct
+nix-store --verify --check-contents
+
+# Try copying back and forth from the store
+nix copy --to file://$cacheDir $outPath
+clearStore
+nix copy --no-check-sigs --from file://$cacheDir $outPath
+# There's no test for that because I don't know how to do that, but at that
+# point we should have the aliases correctly restored
