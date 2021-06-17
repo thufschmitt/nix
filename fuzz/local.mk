4b144179208e265c0f8300b5b670fdbbbba51b89
Pamplemousse <xav.maso@gmail.com>
2021-06-17 09:23:08 -0700

    Fuzzer: Use ASAN
    Signed-off-by: Pamplemousse <xav.maso@gmail.com>

--- fuzz/local.mk
+++ fuzz/local.mk
@@ -1,6 +1,14 @@
 libs = libexpr libstore libutil
 
-fuzz_CXXFLAGS = $(nix_CXXFLAGS) -O1 -fsanitize=fuzzer
+# * `shared-libasan` necessary because of `-Wl,-z,defs` in `mk/libraries.mk`.
+#   See https://github.com/google/sanitizers/wiki/AddressSanitizer#faq ;
+# * `AddressSanitizerUseAfterScope` seems to raise a lot of false positive;
+fuzz_CXXFLAGS = $(nix_CXXFLAGS) \
+								-O1 \
+								-fsanitize=address,fuzzer \
+								-shared-libasan \
+								-fno-sanitize-address-use-after-scope
+
 fuzz_LDFLAGS = $(nix_LDFLAGS) $(foreach l,$(libs),-Lsrc/$(l) $(subst lib,nix,-l$(l)))
 
 fuzzer: fuzz/fuzz_target.cc $(foreach l,$(libs),src/$(l)/$(subst lib,libnix,$(l)).$(SO_EXT))
