c13364aaf75a7731e511b5139a1d13c7c7789a66
Pamplemousse <xav.maso@gmail.com>
2021-06-18 14:29:48 -0700

    Fuzzer: Apply ASAN for shared libraries too
    Signed-off-by: Pamplemousse <xav.maso@gmail.com>

--- scripts/build_fuzzer.sh
+++ scripts/build_fuzzer.sh
@@ -4,4 +4,11 @@ export BUILD_DEBUG=1
 
 ./bootstrap.sh
 ./configure $configureFlags --prefix="$(pwd)/outputs/out"
-make fuzzer CXXFLAGS=-O1 OPTIMIZE=0
+
+# * `shared-libasan` necessary because of `-Wl,-z,defs` in `mk/libraries.mk`.
+#   See https://github.com/google/sanitizers/wiki/AddressSanitizer#faq ;
+# * `AddressSanitizerUseAfterScope` seems to raise a lot of false positive;
+make fuzzer \
+  CXXFLAGS="-O0 -fsanitize=address -fno-sanitize-address-use-after-scope" \
+  LDFLAGS="-fsanitize=address -shared-libasan" \
+  OPTIMIZE=0
